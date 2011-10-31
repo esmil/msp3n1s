21c67982e660cda368300a0f7d55d07bc2afef91
Emil Renner Berthing <esmil@mailme.dk>
2011-11-01 00:00:55 +0100

    serial: update example to use both rx and tx

--- examples/serial/serial.c
+++ examples/serial/serial.c
@@ -16,54 +16,26 @@
  * along with msp3n1s.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/*
+ * This "parrot program" just echoes back whatever is send
+ * to it over the serial line. Type
+ *
+ * $ make screen
+ *
+ * to test it (and "<Ctrl-A>:quit<Enter>" to exit screen).
+ */
+
 #include <watchdog.h>
 #include <clock.h>
 #include <pins.h>
 
 #define LED1 1.0
 #define LED2 1.6
-#define S2   1.3
 
-#define SERIAL_PRINTF_X
-#define SERIAL_PRINTF_D
-#define SERIAL_PRINTF_S
 #include "lib/serial_tx.c"
 
-#define BLINKTIME 0xFFFF
-
-static void
-port1_interrupt(void)
-{
-	pin_interrupt_disable(S2); /* debounce */
-	pin_interrupt_clear(S2);
-
-	watchdog_timer_8ms();
-	watchdog_timer_interrupt_clear();
-	watchdog_timer_interrupt_enable();
-
-	LPM0_EXIT;
-}
-
-static void
-watchdog_interrupt(void)
-{
-	watchdog_timer_interrupt_disable();
-	watchdog_timer_interrupt_clear();
-	watchdog_off();
-
-	pin_interrupt_enable(S2); /* debouncing complete */
-}
-
-static void
-timera_cc1_interrupt(void)
-{
-	timera_cc1 += BLINKTIME;
-
-	pin_toggle(LED1);
-	pin_toggle(LED2);
-
-	timera_cc1_interrupt_clear();
-}
+#define SERIAL_RX_BUFSIZE 32
+#include "lib/serial_rx_buffered.c"
 
 int
 main(void)
@@ -75,32 +47,26 @@ main(void)
 	port1_direction = 0xFF;
 	port1_output = 0xFF;
 
-	/* configure push button */
-	pin_mode_input(S2);
-	pin_high(S2);
-	pin_resistor_enable(S2);
-	pin_interrupt_falling(S2);
-	pin_interrupt_clear(S2);
-	pin_interrupt_enable(S2);
-
+	/* initialize serial clock, tx and rx parts */
 	serial_init_clock();
 	serial_init_tx();
+	serial_init_rx();
 
 	/* enable interrupts */
 	__eint();
 
-	/* start blinking */
-	pin_high(LED1);
-	pin_low(LED2);
-	timera_cc1 = timera_count + BLINKTIME;
-	timera_cc1_interrupt_enable();
+	pin_low(LED1);
+	pin_high(LED2);
 
 	while (1) {
-		LPM0; /* wait for button press */
-#if 0
-		serial_puts("Hello world!\n");
-#else
-		serial_printf("Hello %s, %x %d!\n", "world", 17, -309);
-#endif
+		unsigned char c;
+
+		pin_toggle(LED1);
+		pin_toggle(LED2);
+
+		c = serial_getchar();
+		serial_putchar(c);
+		if (c == '\r')
+			serial_putchar('\n');
 	}
 }

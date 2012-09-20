0029ff0056f9d7b328a9d3404d8888a4cb533ac4
Emil Renner Berthing <esmil@mailme.dk>
2012-09-20 11:42:01 +0200

    macros: make __bi{s,c}_{w,b} macros more correct

--- headers/macros.h
+++ headers/macros.h
@@ -1,10 +1,10 @@
 #ifndef _MACROS_H
 #define _MACROS_H
 
-#define __bis_w(sfr, mask) __asm__ __volatile__("bis.w %0, %1"::"i" (mask), "im" (sfr))
-#define __bic_w(sfr, mask) __asm__ __volatile__("bic.w %0, %1"::"i" (mask), "im" (sfr))
+#define __bis_w(sfr, mask) __asm__ __volatile__("bis.w %1, %0":"=m" (sfr):"ir" (mask))
+#define __bic_w(sfr, mask) __asm__ __volatile__("bic.w %1, %0":"=m" (sfr):"ir" (mask))
 
-#define __bis_b(sfr, mask) __asm__ __volatile__("bis.b %0, %1"::"i" (mask), "im" (sfr))
-#define __bic_b(sfr, mask) __asm__ __volatile__("bic.b %0, %1"::"i" (mask), "im" (sfr))
+#define __bis_b(sfr, mask) __asm__ __volatile__("bis.b %1, %0":"=m" (sfr):"ir" (mask))
+#define __bic_b(sfr, mask) __asm__ __volatile__("bic.b %1, %0":"=m" (sfr):"ir" (mask))
 
 #endif
